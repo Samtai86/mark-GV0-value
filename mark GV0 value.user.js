657b5cc79a4d5c23ce49eba116c3521850727e89
Samtai86 <126457151+Samtai86@users.noreply.github.com>
2023-03-09 18:07:04 +0900

    Update mark GV0 value.user.js

---
+++ mark GV0 value.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         mark GV0 value
+// @namespace    mark_gv0_value
+// @version      1
+// @description  try to reduce a miss of mapping the GV0 values
+// @author       ushentai@
+// @match        https://enumeration-discovery*.amazon.com/*
+// @include     https://enumeration-discovery*.amazon.com/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://gist.github.com/BrockA/2625891/raw/waitForKeyElements.js
+// @downloadURL  https://drive.corp.amazon.com/view/GCO%20Tools/mark_gv0_value.user.js
+// @updateURL    https://drive.corp.amazon.com/view/GCO%20Tools/mark_gv0_value.user.js
+// @run-at       document-start
+// ==/UserScript==
+
+
+(function() {
+  'use strict';
+
+  waitForKeyElements('.search-results', refresh);
+
+  function refresh() {
+   $('td[tabindex]').filter(function() {
+  return $(this).attr('tabindex') % 8 === 0 && $(this).find('div').text() === '0.0';
+}).closest('tr').css({"background-color": "#F0F8FF"}); //Color can be changed in the "" like "yellow"
+  }
+
+  var amstypeinterval = window.setInterval(function(){
+    refresh();
+  }, 600);
+
+})();
