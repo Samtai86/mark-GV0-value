eb7ccfb64e5727d1b76b611957f138578e88c210
Samtai86 <126457151+Samtai86@users.noreply.github.com>
2023-03-10 17:56:09 +0900

    Update mark GV0 value.user.js

--- mark GV0 value.user.js
+++ mark GV0 value.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         mark GV0 value
-// @namespace    mark_gv0_value
-// @version      1
+// @name         mark GV0 value 2
+// @namespace    mark_gv0_value2
+// @version      2
 // @description  try to reduce a miss of mapping the GV0 values
 // @author       ushentai@
 // @match        https://enumeration-discovery*.amazon.com/*
@@ -9,8 +9,8 @@
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://gist.github.com/BrockA/2625891/raw/waitForKeyElements.js
-// @downloadURL  https://drive.corp.amazon.com/view/GCO%20Tools/mark_gv0_value.user.js
-// @updateURL    https://drive.corp.amazon.com/view/GCO%20Tools/mark_gv0_value.user.js
+// @downloadURL  https://drive.corp.amazon.com/view/GCO%20Tools/mark_gv0_value2.user.js
+// @updateURL    https://drive.corp.amazon.com/view/GCO%20Tools/mark_gv0_value2.user.js
 // @run-at       document-start
 // ==/UserScript==
 
@@ -21,9 +21,18 @@
   waitForKeyElements('.search-results', refresh);
 
   function refresh() {
-   $('td[tabindex]').filter(function() {
-  return $(this).attr('tabindex') % 8 === 0 && $(this).find('div').text() === '0.0';
-}).closest('tr').css({"background-color": "#F0F8FF"}); //Color can be changed in the "" like "yellow"
+  var isSynOrValid = false;
+  $('td').filter(function() {
+  var tabIndex = $(this).attr('tabindex');
+  if (tabIndex % 8 === 3 && $(this).find('span').text().match(/^(Synonym|Valid)$/i) && !isSynOrValid) {
+    $(this).closest('tr').css('background-color', 'transparent');
+    isSynOrValid = true;
+  }
+  if (tabIndex % 8 === 0 && $(this).find('div').text().trim() === '0.0' && !isSynOrValid) {
+    $(this).closest('tr').css('background-color', '#F0F8FF');
+  }
+});
+
   }
 
   var amstypeinterval = window.setInterval(function(){
